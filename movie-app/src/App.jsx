376c39cab908903a9ef8d218031e1f23d9c50dcd
codewithEshaYoutube <guesswhatans@gmail.com>
2026-02-15 02:11:48 +0500

    late night git push

--- movie-app/src/App.jsx
+++ movie-app/src/App.jsx
@@ -1,85 +1,102 @@
-
 import Search from './components/Search';
-import { useEffect ,useState} from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
-const API_BASE_URL='https://api.themoviedb.org/3';
-const API_KEY=import.meta.env.VITE_TMDB_API_KEY;
-const API_OPTIONS={
-  method:'GET',
-  headers:{
-    accept:'application/json',
-    Authorization:`Bearer ${API_KEY}`,
-
-  },
-};
-const App =()=>{
-  const [searchTerm,setSearchTerm]= useState("");
-  const [errorMessage, setErrorMessage]=useState('');
-  const [movieList, setMovieList]=useState([]);
-  const [isLoading , setIsLoading]=useState(initialState,true);
-  const fetchMovies =async()=>{
-    setIsLoading(true);
-    setErrorMessage(''); 
-  try {
-    const endpoint=`${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
-    const response = await fetch(endpoint, API_OPTIONS);
-    
-    if(!response.ok){
-      throw new Error("Failed to fetch movies");
-    }
-
-const data=await response.json();
-setMovieList(data.results || []);
-  }catch (error){
-    console.error(`Error fetching movies : ${error}`);
-    setErrorMessage("Error fetching movies. Please try again later. ");
-  }finally{
-    setIsLoading(false);
+const API_BASE_URL = 'https://api.themoviedb.org/3';
+const API_KEY = import.meta.env.vite_apikey;
+const API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlZjY5YTE5YjBiZmU4YjcyNzZkNDIxMTVkMjFkMDYwNiIsIm5iZiI6MTc3MDI5Mzg0MC45ODEsInN1YiI6IjY5ODQ4YTUwNmQwNjFmYzczMjZlNzZlOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.fIHQFdblGTdXtYFgCTd5S0GY-rvO1w3DXjaSpvxxv4k'
   }
 };
-  
-  
-  useEffect(() =>{
-   fetchMovies();
-  }, []);
 
 
-  return(
-    <main>
-    <div className="pattern"/>
-    <div className="wrapper">
-      <header>
-      <img src="./hero-img.png" alt="Hero-Banner"/>
-      <h1>Find <span className="text-gradient" >Movies</span>  You'll Enjoy Without the Hassle</h1>
-    
-     
-    <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
-    </header>
-    <section className='all-movies'>
-      <h2>All Movies</h2>
-      {isLoading ? (
-        <p className='text-white'>Loading...</p>
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [errorMessage, setErrorMessage] = useState('');
+  const [movieList, setMovieList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
+  const fetchMovies = async () => {
+    setIsLoading(true);
+    setErrorMessage('');
 
-      ): errorMessage ? (
-        <p className='text-red-500'>{errorMessage}</p>
-      ):(
-        <ul>
-          {movieList.map((movie)=>(
-          <p className="text-white">{movie.title}</p>
-
-          ))}
-        </ul>
+    try {
+      const endpoint = searchTerm
       
-      )}
+        ? `${API_BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(searchTerm)}`
+        : `${API_BASE_URL}/discover/movie?api_key=${API_KEY}&sort_by=popularity.desc`;
 
-    </section>
-    
-    
-    </div>
+      const response = await fetch(endpoint,API_OPTIONS);
+
+      if (!response.ok) {
+        throw new Error("Failed to fetch movies");
+      }
+
+      const data = await response.json();
+      setMovieList(data.results || []);
+    } catch (error) {
+      setErrorMessage("Error fetching movies. Please try again later.");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+
+
+  useEffect(() => {
+    fetchMovies();
+  }, [searchTerm]);
+
+  return (
+    <main>
+      <div className="pattern" />
+      <div className="wrapper">
+        <header>
+          <img src="./hero-img.png" alt="Hero-Banner" />
+          <h1>
+            Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle
+          </h1>
+
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        </header>
+
+        <section className="all-movies">
+          <h2 className="mt-[40px]">All Movies</h2>
+
+          {isLoading ? (
+            <spinner />
+          ) : errorMessage ? (
+            <p className="text-red-500">{errorMessage}</p>
+          ) : (
+            <ul>
+              {movieList.map((movie) => (
+              <moviecard key={movie.id} movie={movie}/>
+
+              ))}
+            </ul>
+          )}
+        </section>
+      </div>
     </main>
   );
 };
 
 export default App;
+const url = 'https://api.themoviedb.org/3/authentication';
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlZjY5YTE5YjBiZmU4YjcyNzZkNDIxMTVkMjFkMDYwNiIsIm5iZiI6MTc3MDI5Mzg0MC45ODEsInN1YiI6IjY5ODQ4YTUwNmQwNjFmYzczMjZlNzZlOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.fIHQFdblGTdXtYFgCTd5S0GY-rvO1w3DXjaSpvxxv4k'
+  }
+};
+
+fetch(url, options)
+  .then(res => res.json())
+  .then(json => console.log(json))
+  .catch(err => console.error(err));
+
+
---
+++ movie-app/src/components/moviecard.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const moviecard = ({movie:{ title
+    ,vote_average,poster_path,release_date,orignal_language
+}}) => {
+  return (
+    <div className="moviecard">
+        <img 
+        src={poster_path ? 
+        `https://image.tmdb.org/t/p/w500/${poster_path}` :'/noposter.png/'>
+    </div>
+  )
+}
+
+export default moviecard
